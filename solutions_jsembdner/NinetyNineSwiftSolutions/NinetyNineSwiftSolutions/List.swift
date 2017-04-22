fd0df897f734ce5f2292f86909be44fc251f7794
jsembdner <jan.sembdner@gmail.com>
2017-04-22 17:19:55 +0200

    Set up Framework with Unit Tests for solutions

---
+++ solutions_jsembdner/NinetyNineSwiftSolutions/NinetyNineSwiftSolutions/List.swift
@@ -0,0 +1,18 @@
+
+public class List<T> {
+	var value: T!
+	var nextItem: List<T>?
+	
+	public convenience init!(_ values: T...) {
+		self.init(Array(values))
+	}
+	
+	init!( _ values: Array<T>) {
+		var values = values
+		if values.count == 0 {
+			return nil
+		}
+		value = values.removeFirst()
+		nextItem = List(values)
+	}
+}
---
+++ solutions_jsembdner/NinetyNineSwiftSolutions/NinetyNineSwiftSolutions/NinetyNineSwiftSolutions.h
@@ -0,0 +1,19 @@
+//
+//  NinetyNineSwiftSolutions.h
+//  NinetyNineSwiftSolutions
+//
+//  Created by Jan Sembdner on 22.04.17.
+//  Copyright © 2017 Jan Sembdner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NinetyNineSwiftSolutions.
+FOUNDATION_EXPORT double NinetyNineSwiftSolutionsVersionNumber;
+
+//! Project version string for NinetyNineSwiftSolutions.
+FOUNDATION_EXPORT const unsigned char NinetyNineSwiftSolutionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NinetyNineSwiftSolutions/PublicHeader.h>
+
+
